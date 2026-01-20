1270ae82975e648f1f1d38911bf4e279d613fe98
Mister-Lasser <jayechouhan845@gmail.com>
2026-01-20 12:36:45 +0530

    added support for HTTPS

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,39 @@
+#define BOOST_ASIO_USE_OPENSSL
+
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 #include <string>
 #include <string_view>
 #include <cassert>
 #include <array>
 #include <exception>
-#include <fstream>
 #include <map>
 #include <cctype>
 #include <algorithm>
+#include <boost/asio/ssl/stream_base.hpp>
+#include <openssl/ssl.h>
+#include <windows.h>
+#include <shellapi.h>
+
 
 namespace Helper
 {
 	std::string strip(const std::string& input)
 	{
-		auto start_it{ input.begin() };
-		auto end_it{ input.rbegin() };
+		auto start_it = input.begin();
+		auto end_it = input.end();
 
-		while (std::isspace(static_cast<unsigned char>(*start_it))) 
+		while (start_it != end_it &&
+			(std::isspace((unsigned char)*start_it) || *start_it == '\r'))
 			++start_it;
 
-		if (start_it != input.end())
-		{
-			while (std::isspace(*end_it)) ++end_it;
-		}
+		while (end_it != start_it &&
+			std::isspace((unsigned char)*(end_it - 1)))
+			--end_it;
 
-		return std::string(start_it, end_it.base());
+		return std::string(start_it, end_it);
+
 	}
 }
 
@@ -65,9 +73,14 @@ public:
 		m_path = sv.substr(slash_pos);
 	}
 
-	void parse_headers(std::map<std::string, std::string>& map, const std::string& content)
+	void parse_headers(auto& map, const std::string& content)
 	{
+		if (content.empty() || content == "\r")
+			return;
+
 		std::size_t pos{ content.find(":") };
+		if (pos == std::string::npos)
+			return;
 	
 		std::string header{ content.substr(0, pos) }, value{ content.substr(pos + 1) };
 		std::transform(
@@ -75,72 +88,109 @@ public:
 			, header.begin()
 			, [](unsigned char c) { return std::tolower(c); }
 		);
-		map.emplace(
-			 header
-			,Helper::strip(value)
-		);
+		map[header] = Helper::strip(value);
 	}
 
-	std::optional<std::string> request()
+	std::optional<std::size_t> getContentLength(
+		const std::map<std::string, std::string>& headers)
 	{
-		boost::asio::io_context ioc;
-		TCP::resolver resolver{ ioc };
-		TCP::resolver::results_type endpoints{ resolver.resolve(m_host, "80") };
-		TCP::socket socket{ ioc };
-
-		boost::asio::connect(socket, endpoints);
-
-		std::string request{ std::format("GET {} HTTP/1.0\r\n", m_path) };
-		request.append(std::format("Host: {}\r\n", m_host));
-		request.append("\r\n");
-
-		boost::asio::write(socket, boost::asio::buffer(request));
-
-		std::array<char, 1024> buf;
-		std::string recvBuf;
-		bool headersParsed{};
-		std::map<std::string, std::string> responseHeaders{};
-		std::string content{};
-		try
+		if (auto it = headers.find("content-length"); it != headers.end())
 		{
-			while (true)
+			return static_cast<std::size_t>(std::stoull(it->second));
+		}
+		return std::nullopt;
+	}
+
+
+	//factoring out request/response logic
+	template <typename Stream>
+	std::string read_response(Stream& stream, auto& responseHeaders)
+	{
+		std::array<char, 1024> buf;
+		std::string recvBuf, content;
+		bool headersParsed{ false };
+
+		std::optional<std::size_t> contentLength;
+		std::size_t bytesRead = 0;
+
+		while (true)
+		{
+			boost::system::error_code ec;
+			std::size_t len = stream.read_some(boost::asio::buffer(buf), ec);
+
+			if (ec == boost::asio::error::eof)
+				break;
+			if (ec)
+				throw boost::system::system_error(ec);
+
+			recvBuf.append(buf.data(), len);
+
+			if (!headersParsed)
 			{
-				boost::system::error_code error;
-
-				std::size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-				if (error == boost::asio::error::eof)
-					break;
-				else if (error)
-					throw boost::system::system_error(error);
-
-				recvBuf.append(buf.data(), len);
-
-				if (!headersParsed)
+				auto pos = recvBuf.find("\r\n\r\n");
+				if (pos != std::string::npos)
 				{
-					std::size_t pos{ recvBuf.find("\r\n\r\n") };
-					if (pos != std::string::npos)
+					std::string headers{  };
+
+					std::istringstream iss{ recvBuf.substr(0, pos) };
+					std::string line;
+
+					//status line
+					if (std::getline(iss, line))
 					{
-						std::string headers{ recvBuf.substr(0, pos) };
+						if (line.ends_with('\r'))
+							line.pop_back();
 
-						std::istringstream iss{ headers };
-						std::string line;
-
-						std::getline(iss, line);
-
-						while (std::getline(iss, line))
-						{
-							if (line.ends_with('\r'))
-								line.pop_back();
-							parse_headers(responseHeaders, line);
-						}
-
-						recvBuf.erase(0, pos + 4);
-						headersParsed = true;
-
-						content.append(recvBuf);
-						recvBuf.clear();
+						if (line.rfind("HTTP/", 0) == 0)
+							responseHeaders["status"] = Helper::strip(line);
 					}
+
+					//status check
+					int status{};
+					if (auto it{ responseHeaders.find("status") }; it != responseHeaders.end())
+					{
+						std::istringstream issm{ it->second };
+						std::string http{};
+						issm >> http >> status;
+
+						if (status / 100 != 2)
+						{
+							throw std::runtime_error(
+								"HTTP request failed: " + it->second
+							);
+						}
+					}
+
+					//headers
+					while (std::getline(iss, line))
+					{
+						if (line.ends_with('\r'))
+							line.pop_back();
+						parse_headers(responseHeaders, line);
+					}
+
+					recvBuf.erase(0, pos + 4);
+					headersParsed = true;
+					contentLength = getContentLength(responseHeaders);
+					content.append(recvBuf);
+					recvBuf.clear();
+				}
+			}
+			else
+			{
+				if (contentLength)
+				{
+					std::size_t toCopy = std::min(
+						recvBuf.size(),
+						*contentLength - bytesRead
+					);
+
+					content.append(recvBuf.data(), toCopy);
+					bytesRead += toCopy;
+					recvBuf.erase(0, toCopy);
+
+					if (bytesRead >= *contentLength)
+						break;
 				}
 				else
 				{
@@ -149,9 +199,94 @@ public:
 				}
 			}
 		}
-		catch (std::exception& e)
+		return content;
+	}
+
+	void formRequest(std::string& request)
+	{
+		request = std::format(
+			"GET {} HTTP/1.0\r\n"
+			"Host: {}\r\n"
+			"Accept-Encoding: identity\r\n"
+			"Connection: close\r\n"
+			"User-Agent: some\r\n"
+			"\r\n",
+			m_path, m_host
+		);
+	}
+	
+
+
+	std::optional<std::string> request()
+	{
+		namespace SSL = boost::asio::ssl;
+		std::map<std::string, std::string> responseHeaders{};
+		std::string content{};
+
+		boost::asio::io_context ioc;
+		TCP::resolver resolver{ ioc };
+
+		std::string_view port{ (m_scheme == "https") ? "443" : "80" };
+		TCP::resolver::results_type endpoints{ resolver.resolve(m_host, port) };
+		TCP::socket socket{ ioc };
+
+		std::string request{};
+		formRequest(request);
+
+		if (m_scheme == "http")
 		{
-			std::cerr << "Exception in thread: " << e.what() << '\n';
+			boost::asio::connect(socket, endpoints);
+			boost::asio::write(socket, boost::asio::buffer(request));
+			content = read_response(socket, responseHeaders);
+		}
+		else
+		{
+			SSL::context ctx{ SSL::context::tls_client };
+
+			//load CA bundle
+			//ctx.set_default_verify_paths();
+			try
+			{
+				ctx.load_verify_file("C:/Program Files/OpenSSL-Win64/cert/cacert.pem");
+			}
+			catch (std::exception& e)
+			{
+				std::cerr << "Error is: " << e.what() << '\n';
+				return std::nullopt;
+			}
+
+
+			SSL::stream<TCP::socket> stream{ ioc, ctx };
+
+			stream.set_verify_mode(SSL::verify_peer);
+			stream.set_verify_callback(
+				SSL::host_name_verification(std::string(m_host))
+			);
+
+			boost::asio::connect(stream.next_layer(), endpoints);
+
+			if (!SSL_set_tlsext_host_name(stream.native_handle(), std::string(m_host).c_str()))
+			{
+				throw boost::system::system_error(
+					static_cast<int>(::ERR_get_error()),
+					boost::asio::error::get_ssl_category()
+				);
+			}
+
+			boost::system::error_code ec;
+			stream.handshake(SSL::stream_base::client, ec);
+			if (ec)
+			{
+				std::cerr << "TLS handshake failed: " << ec.message() << "\n";
+			}
+
+
+			boost::asio::write(stream, boost::asio::buffer(request));
+			content = read_response(stream, responseHeaders);
+
+			boost::system::error_code errC;
+			stream.shutdown(errC);  // ignore eof
+
 		}
 
 		assert(!responseHeaders.contains("transfer-encoding"));
@@ -178,8 +313,14 @@ void show(std::string& body)
 		else if (!inTag)
 			std::cout << ch;
 	}
-
 	std::cout << std::endl;
+}
+
+void load(URL& url)
+{
+	auto body{ url.request() };
+	if (body)
+		show(*body);
 }
 
 int main(int argc, char* argv[])
@@ -192,8 +333,7 @@ int main(int argc, char* argv[])
 
 	URL url{ argv[1] };
 	
-	auto body{ url.request() };
-	if (body)
-		show(*body);
-	//std::cout << *url.request() << '\n';
+	load(url);
+
+	return 0;
 }
